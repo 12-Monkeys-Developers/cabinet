14e51d26d9550c5656d54aabf23df0507e222671
khaali-dev <github@jgol.fr>
2023-09-18 14:00:48 +0200

    sheet lock

--- module/applications/sheets/actor.mjs
+++ module/applications/sheets/actor.mjs
@@ -17,7 +17,8 @@ export default class CabinetActorSheet extends ActorSheet {
   async getData(options) {
     const context = {};
 
-    const isEditable = this.isEditable;
+    
+    const isEditable = this.actor.isUnlocked;
     context.cssClass = isEditable ? "editable" : "locked";
     context.editable = isEditable;
 
@@ -25,5 +26,27 @@ export default class CabinetActorSheet extends ActorSheet {
     context.system = this.document.system;
 
     return context;
+  }
+  
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Lock/Unlock la fiche
+    html.find(".sheet-change-lock").click(this._onSheetChangelock.bind(this));
+  }
+
+  /**
+   * Manage the lock/unlock button on the sheet
+   *
+   * @name _onSheetChangelock
+   * @param {*} event
+   */
+  async _onSheetChangelock(event) {
+    event.preventDefault();
+
+    let flagData = await this.actor.getFlag(game.system.id, "SheetUnlocked");
+    if (flagData) await this.actor.unsetFlag(game.system.id, "SheetUnlocked");
+    else await this.actor.setFlag(game.system.id, "SheetUnlocked", "SheetUnlocked");
+    this.actor.sheet.render(true);
   }
 }
--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -1,2 +1,7 @@
 export default class CabinetActor extends Actor {
+
+    get isUnlocked() {
+      if (this.getFlag(game.system.id, "SheetUnlocked")) return true;
+      return false;
+    }
 }
