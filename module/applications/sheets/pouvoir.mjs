d7a652116658b0a305a172510e5ba02cc17b3453
khaali-dev <github@jgol.fr>
2023-10-17 14:58:20 +0200

    pouvoir sheet

--- module/applications/sheets/pouvoir.mjs
+++ module/applications/sheets/pouvoir.mjs
@@ -7,11 +7,30 @@ export default class PouvoirSheet extends CabinetItemSheet {
    */
   static itemType = "pouvoir";
 
+  static get defaultOptions() {
+    const options = super.defaultOptions;
+    return Object.assign(options, {
+      width: 500,
+      height: 500,
+      classes: [SYSTEM.id, "sheet", "item", this.itemType],
+      template: `systems/${SYSTEM.id}/templates/sheets/${this.itemType}.hbs`,
+      resizable: false,
+      tabs: [],
+      scrollY: []
+    });
+  }
+
   /** @override */
   async getData(options) {
     const context = await super.getData(options);
 
+    context.descriptionhtml = await TextEditor.enrichHTML(this.item.system.description, { async: false });
     context.spheres = SYSTEM.SPHERES;
+    context.imgsphere = "";
+    if (game.modules.get("cabinet-livredebase")){
+      context.imgsphere = `modules/cabinet-livredebase/assets/images/illustrations/spheres/${this.item.system.sphere}.webp`;
+    }
     return context;
   }
 }
+
