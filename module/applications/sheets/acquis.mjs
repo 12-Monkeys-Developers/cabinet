5aad1ce01edaaae35c0ad7886005bd85a78267b6
khaali-dev <github@jgol.fr>
2023-10-22 21:47:41 +0200

    sheet acquis

--- module/applications/sheets/acquis.mjs
+++ module/applications/sheets/acquis.mjs
@@ -1,6 +1,14 @@
 import CabinetItemSheet from "./item.mjs";
 
 export default class AcquisSheet extends CabinetItemSheet {
+  static get defaultOptions() {
+    const options = super.defaultOptions;
+    return Object.assign(options, {
+      height: 400,
+      width: 600,
+      resizable: false,
+    });
+  }
   /**
    * Le type d'Item qu'affiche cette Sheet
    * @type {string}
@@ -12,6 +20,7 @@ export default class AcquisSheet extends CabinetItemSheet {
     const context = await super.getData(options);
 
     context.spheres = SYSTEM.SPHERES;
+    context.descriptionhtml = await TextEditor.enrichHTML(this.item.system.description, { async: false });
     return context;
   }
 }
