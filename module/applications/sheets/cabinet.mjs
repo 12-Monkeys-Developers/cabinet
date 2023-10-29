64c43a6ec445433bfa4f2df7249ddf7a1f19ef29
khaali-dev <github@jgol.fr>
2023-10-29 11:00:19 +0100

    context menu sheet cabinet

--- module/applications/sheets/cabinet.mjs
+++ module/applications/sheets/cabinet.mjs
@@ -21,7 +21,6 @@ export default class CabinetSheet extends CabinetActorSheet {
   async getData(options) {
     const context = await super.getData(options);
     context.esprits = this.actor.listeEsprits;
-
     // Acquis par ordre alpha et mise en forme de la description
     context.acquis = this.actor.items
       .filter((item) => item.type == "acquis")
@@ -31,6 +30,7 @@ export default class CabinetSheet extends CabinetActorSheet {
     context.acquis.forEach((element) => {
       element.system.descriptionhtml = TextEditor.enrichHTML(element.system.description, { async: false });
     });
+    context.isgm=game.user.isGM;
 
     return context;
   }
@@ -40,6 +40,67 @@ export default class CabinetSheet extends CabinetActorSheet {
     super.activateListeners(html);
 
     html.find(".select-cabinet").click(this._onSelectCabinet.bind(this));
+   
+    // Activate context menu
+    this._contextMenu(html);
+  }
+
+
+  /** @inheritdoc */
+  _contextMenu(html) {
+    ContextMenu.create(this, html, ".membre-contextmenu", this._getEntryContextOptions());
+  }
+
+  /**
+   * Retourne les context options du menu 
+   * @returns {object[]}   
+   * @private
+   */
+  _getEntryContextOptions() {
+    return [
+      {
+        name: `Aller dans son Jardin Secret`,
+        icon: `<i class="fa-regular fa-face-clouds"></i>`,
+        condition: li => {
+          const actorId=li.data("actorId");
+          const actor=game.actors.get(actorId);
+          if(!actor)return false;
+          return !actor.system.jardin;
+        },
+        callback: li => {
+          const actorId=li.data("actorId");
+          this._onAllerJardin(actorId);
+        }
+      },
+      {
+        name: `Revenir dans le cabinet`,
+        icon: `<i class="fa-regular fa-loveseat"></i>`,
+        condition: li => {
+          const actorId=li.data("actorId");
+          const actor=game.actors.get(actorId);
+          if(!actor)return false;
+          return actor.system.jardin;
+        },
+        callback: li => {
+          const actorId=li.data("actorId");
+          this._onQuitterJardin(actorId);
+        }
+      },
+      {
+        name: `Nommer comedien`,
+        icon: `<i class="fa-solid fa-person-simple"></i>`,
+        condition: li => {
+          const actorId=li.data("actorId");
+          const actor=game.actors.get(actorId);
+          if(!actor)return false;
+          return !actor.system.comedien;
+        },
+        callback: li => {
+          const actorId=li.data("actorId");
+          this._onNommerComedien(actorId);
+        }
+      },
+    ];
   }
 
   /**
@@ -56,5 +117,36 @@ export default class CabinetSheet extends CabinetActorSheet {
     let cabinetId = event.currentTarget.dataset.actorId;
     await game.settings.set("cabinet", "cabinet", cabinetId);
     await this.actor.update({ "ownership.default": 3 });
+  }
+
+  async _onAllerJardin(actorId) {
+    const actor=game.actors.get(actorId);
+    if(!actor)return;
+    await actor.allerJardin(true);
+    this.render();
+  }
+
+  async _onQuitterJardin(actorId) {
+    const actor=game.actors.get(actorId);
+    if(!actor)return;
+    await actor.quitterJardin();
+    this.render();
+  }
+
+  async _onNommerComedien(actorId) {
+    const newComedien=game.actors.get(actorId);
+    if(!newComedien)return;
+    let oldComedien = await game.actors.get(this.actor.system.comedien);
+    if(oldComedien){
+      await oldComedien.update({ "system.comedien": false });
+    }
+    if(newComedien.system.jardin){
+      newComedien.quitterJardin();
+    }
+    await newComedien.update({ "system.comedien": true });
+    await this.actor.majComedien(actorId);
+    const allowed = Hooks.call("cabinet.changementComedien", actorId);
+    if (allowed === false) return;
+    this.render();
   }
 }
