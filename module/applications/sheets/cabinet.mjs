f74c430208d804d0f57a7e767512541a053de214
Christophe Laudon <ch.laudon@gmail.com>
2023-10-30 08:57:23 +0100

    Méthode ajouterEsprit dans le cabinet
    Déplacement de la méthode de la sheet vers l'actor

--- module/applications/sheets/cabinet.mjs
+++ module/applications/sheets/cabinet.mjs
@@ -36,7 +36,7 @@ export default class CabinetSheet extends CabinetActorSheet {
       element.system.descriptionhtml = TextEditor.enrichHTML(element.system.description, { async: false });
     });
     context.corps = game.actors.get(this.actor.system.corps);
-    context.isgm=game.user.isGM;
+    context.isgm = game.user.isGM;
 
     return context;
   }
@@ -46,83 +46,82 @@ export default class CabinetSheet extends CabinetActorSheet {
     super.activateListeners(html);
 
     html.find(".select-cabinet").click(this._onSelectCabinet.bind(this));
-   
+
     // Activate context menu
     this._contextMenu(html);
   }
 
-
   /** @inheritdoc */
   _contextMenu(html) {
-    ContextMenu.create(this, html, ".membre-contextmenu", this._getEntryContextOptions());
+    ContextMenu.create(this, html, ".membre-contextmenu", this._getMembreEntryContextOptions());
     ContextMenu.create(this, html, ".corps-contextmenu", this._getCorpsEntryContextOptions());
   }
 
   /**
-   * Retourne les context options du menu membres
-   * @returns {object[]}   
+   * Retourne les context options du menu Membre
+   * @returns {object[]}
    * @private
    */
-  _getEntryContextOptions() {
+  _getMembreEntryContextOptions() {
     return [
       {
         name: `Aller dans son Jardin Secret`,
         icon: `<i class="fa-regular fa-face-clouds"></i>`,
-        condition: li => {
-          const actorId=li.data("actorId");
-          const actor=game.actors.get(actorId);
-          if(!actor)return false;
+        condition: (li) => {
+          const actorId = li.data("actorId");
+          const actor = game.actors.get(actorId);
+          if (!actor) return false;
           return !actor.system.jardin;
         },
-        callback: li => {
-          const actorId=li.data("actorId");
+        callback: (li) => {
+          const actorId = li.data("actorId");
           this._onAllerJardin(actorId);
-        }
+        },
       },
       {
         name: `Revenir dans le cabinet`,
         icon: `<i class="fa-regular fa-loveseat"></i>`,
-        condition: li => {
-          const actorId=li.data("actorId");
-          const actor=game.actors.get(actorId);
-          if(!actor)return false;
+        condition: (li) => {
+          const actorId = li.data("actorId");
+          const actor = game.actors.get(actorId);
+          if (!actor) return false;
           return actor.system.jardin;
         },
-        callback: li => {
-          const actorId=li.data("actorId");
+        callback: (li) => {
+          const actorId = li.data("actorId");
           this._onQuitterJardin(actorId);
-        }
+        },
       },
       {
         name: `Nommer comedien`,
         icon: `<i class="fa-solid fa-person-simple"></i>`,
-        condition: li => {
-          const actorId=li.data("actorId");
-          const actor=game.actors.get(actorId);
-          if(!actor)return false;
+        condition: (li) => {
+          const actorId = li.data("actorId");
+          const actor = game.actors.get(actorId);
+          if (!actor) return false;
           return !actor.system.comedien;
         },
-        callback: li => {
-          const actorId=li.data("actorId");
+        callback: (li) => {
+          const actorId = li.data("actorId");
           this._onNommerComedien(actorId);
-        }
+        },
       },
       {
         name: `Enlever`,
         icon: `<i class="fa-solid fa-trash"></i>`,
         condition: true,
-        callback: li => {
-          const actorPosition=li.data("index");
-          const actorId=li.data("actorId");
-          this._enleverMembre(actorId, actorPosition);
-        }
+        callback: (li) => {
+          const actorPosition = li.data("index");
+          const actorId = li.data("actorId");
+          this._onEnleverMembre(actorId, actorPosition);
+        },
       },
     ];
   }
 
   /**
-   * Retourne les context options du menu membres
-   * @returns {object[]}   
+   * Retourne les context options du menu Corps
+   * @returns {object[]}
    * @private
    */
   _getCorpsEntryContextOptions() {
@@ -130,20 +129,20 @@ export default class CabinetSheet extends CabinetActorSheet {
       {
         name: `Inconscience`,
         icon: `<i class="fa-regular fa-face-clouds"></i>`,
-        condition: li => {
+        condition: (li) => {
           return this.actor.system.corps ? true : false;
         },
-        callback: li => {
+        callback: (li) => {
           this._endormirCorps();
-        }
+        },
       },
       {
         name: `Enlever`,
         icon: `<i class="fa-solid fa-trash"></i>`,
         condition: true,
-        callback: li => {
+        callback: (li) => {
           this._enleverCorps();
-        }
+        },
       },
     ];
   }
@@ -165,33 +164,33 @@ export default class CabinetSheet extends CabinetActorSheet {
   }
 
   async _onAllerJardin(actorId) {
-    const actor=game.actors.get(actorId);
-    if(!actor)return;
+    const actor = game.actors.get(actorId);
+    if (!actor) return;
     await actor.deplacerPosition(null, true);
     this.render();
   }
 
   async _onQuitterJardin(actorId) {
-    const actor=game.actors.get(actorId);
-    if(!actor)return;
+    const actor = game.actors.get(actorId);
+    if (!actor) return;
     await actor.deplacerPosition("auto", true);
     this.render();
   }
 
   async _onNommerComedien(actorId) {
-    const actor=game.actors.get(actorId);
-    if(!actor)return;
+    const actor = game.actors.get(actorId);
+    if (!actor) return;
     await this.actor.majComedien(actorId);
     this.render();
   }
 
   /**
    * Retire un membre de cabinet
-   * @param {*} id 
+   * @param {*} id
    */
-  async _enleverMembre(actorId, actorPosition) {
+  async _onEnleverMembre(actorId, actorPosition) {
     //remet d'abord l'esprit dans son jardin pour tout nettoyer
-    let esprit=game.actors.get(actorId);
+    let esprit = game.actors.get(actorId);
     await esprit.deplacerPosition(null, true);
     //puis supprime l'esprit du cabinet
     let esprits = this.actor.system.esprits;
@@ -199,7 +198,7 @@ export default class CabinetSheet extends CabinetActorSheet {
     await this.actor.update({ "system.esprits": esprits });
     this.render();
   }
-  
+
   /** @override */
   async _onDropActor(event, data) {
     const actor = await fromUuid(data.uuid);
@@ -209,12 +208,7 @@ export default class CabinetSheet extends CabinetActorSheet {
 
     // Drop d'esprit
     if (actor.type === "esprit") {
-      let esprits = this.actor.system.esprits;
-      if (esprits.includes(actorId)) return false;
-      esprits.push(actorId);
-      await this.actor.update({ "system.esprits": esprits });
-      await actor.update({ "system.jardin": true });
-      await actor.update({ "system.comedien": false });
+      this.actor.ajouterEsprit(actor);
     }
 
     // Drop de corps
@@ -226,8 +220,8 @@ export default class CabinetSheet extends CabinetActorSheet {
   }
 
   async _endormirCorps() {
-    for(let membreId of this.actor.system.esprits){
-      const membre=game.actors.get(membreId);
+    for (let membreId of this.actor.system.esprits) {
+      const membre = game.actors.get(membreId);
       await membre.deplacerPosition(null, true);
     }
     this.render();
