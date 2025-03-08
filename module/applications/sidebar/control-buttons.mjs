41dcdf76ccdb4a6d79fef19052464bc59b68c071
khaali-dev <github@jgol.fr>
2025-03-08 21:28:24 +0100

    sidebar

--- module/applications/sidebar/control-buttons.mjs
+++ module/applications/sidebar/control-buttons.mjs
@@ -2,42 +2,37 @@ import { ArbreVieForm } from "../forms/arbre-vie.mjs";
 import { PresentationForm } from "../forms/presentation.mjs";
 
 export default function initControlButtons() {
-  CONFIG.Canvas.layers.cabinet = { layerClass: ControlsLayer, group: "primary" };
-
-  Hooks.on("getSceneControlButtons", (btns) => {
-    let menu = [];
-
-    menu.push(
-      {
-        name: "arbre",
-        title: "Arbre de vie",
-        icon: "logo_defaut",
-        button: true,
-        onClick: () => {
-          const cabinet = game.actors.filter((actor) => actor.type === "cabinet")[0];
-          if (!cabinet) {
-            return ui.notifications.warn("L'arbre de vie nécessite un cabinet actif !");
-          } else {
-            if (cabinet) new ArbreVieForm(cabinet).render(true);
-          }
-        },
-      },
-      {
-        name: "presentation",
-        title: "Présentation du système",
-        icon: "fas fa-question",
-        button: true,
-        onClick: () => {
-          new PresentationForm().render(true);
-        },
-      }
-    );
-    btns.push({
+  Hooks.on("getSceneControlButtons", (controls) => {
+    controls.cabinet = {
       name: "cabinet_menu",
       title: "Le cabinet des murmures",
       icon: "logo_comportement",
-      layer: "cabinet",
-      tools: menu,
-    });
+      tools: {
+        arbre: {
+          name: "arbre",
+          title: "Arbre de vie",
+          icon: "logo_defaut",
+          onChange: (event, active) => {
+            const cabinet = game.actors.filter((actor) => actor.type === "cabinet")[0];
+            if (!cabinet) {
+              return ui.notifications.warn("L'arbre de vie nécessite un cabinet actif !");
+            } else {
+              if (cabinet) new ArbreVieForm(cabinet).render(true);
+            }
+          },
+          button: true,
+        },
+        presentation: {
+          name: "presentation",
+          title: "Présentation du système",
+          icon: "fas fa-question",
+          button: true,
+          onChange: (event, active) => {
+            new PresentationForm().render(true);
+          },
+          button: true,
+        },
+      },
+    };
   });
 }
